ad1c04de865c233be65c3417f7c63c607b0d035b
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-07-12 15:35:32 +0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -12,10 +9,14 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
