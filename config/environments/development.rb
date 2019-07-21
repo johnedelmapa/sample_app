4c1fa61ecb374c36406ea937c36ee9ef0e1763c2
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-07-21 22:21:15 +0800

    Heroku config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'   
+  host = 'sheltered-hamlet-35595.herokuapp.com'   
   # Use this on the cloud IDE.
   config.action_mailer.default_url_options = { host: host }
 
